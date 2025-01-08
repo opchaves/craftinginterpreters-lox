2394c54973f043f78bc9001d09b947c37d0710d9
Paulo Chaves <opaulochaves@gmail.com>
2025-01-08 10:41:38 -0300

    scan keywords

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -11,6 +13,28 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 0;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -100,11 +124,32 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphanumeric(peek()))
+      advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null)
+      type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private boolean isAlphanumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
   }
 
   private boolean isDigit(char c) {
