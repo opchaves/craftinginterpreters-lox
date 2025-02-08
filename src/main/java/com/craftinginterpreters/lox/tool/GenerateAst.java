0e6dcb4488dd757edbf1f8547d7407a6cf0a16dd
Paulo Chaves <opaulochaves@gmail.com>
2025-02-08 15:33:39 -0300

    implement visitor pattern

--- src/main/java/com/craftinginterpreters/lox/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/lox/tool/GenerateAst.java
@@ -30,6 +30,13 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " { ");
 
+    defineVisitor(writer, baseName, types);
+
+    // The base accept() method
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+    writer.println();
+
     // The AST classes.
     for (String type : types) {
       String className = type.split(":")[0].trim();
@@ -39,6 +46,17 @@ public class GenerateAst {
 
     writer.println("}");
     writer.close();
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
   }
 
   private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -54,6 +72,13 @@ public class GenerateAst {
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    // Visitor pattern
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
 
     // Fields
